de3b333b5cd763a634657017e8dc5dd3e7364760
Christoph Hoffmann <Christoph.Hoffmann@adesso.de>
2018-11-22 22:08:33 +0100

    integrate web viewer in cli

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,107 +1,124 @@
 #!/usr/bin/env node
 import * as yargs from 'yargs';
 import {run} from '../main';
+import {runServer} from './server';
+import {Arguments, Argv} from 'yargs';
 
 const argv = yargs
-  .option('i', {
-    alias: 'input',
-    description: 'dependency-cruiser JSON file',
-    demandOption: true,
-    type: 'string',
-    requiresArg: true
-  })
-  .option('f', {
-    alias: 'filter',
-    description: 'path for the dependency subgraph',
-    default: '',
-    type: 'string',
-    requiresArg: true
-  })
-  .option('d', {
-    alias: 'depth',
-    description: 'number of child folders to show(0 for infinite)',
-    default: 1,
-    type: 'number',
-    requiresArg: true
-  })
-  .option('D', {
-    alias: 'external-depth',
-    description: 'number of child folders for external dependencies',
-    default: 1,
-    type: 'number',
-    requiresArg: true
-  })
-  .option('e', {
-    alias: 'external-dependencies',
-    description: 'flag to show dependencies which point outside of <filter>',
-    default: false,
-    type: 'boolean',
-  })
-  .option('F', {
-    alias: 'external-filter',
-    description: 'filter path for external dependencies and dependents',
-    type: 'string',
-  })
-  .option('E', {
-    alias: 'external-dependents',
-    description: 'flag to show dependents which point inside of <filter>',
-    default: false,
-    type: 'boolean',
-  })
-  .option('C', {
-    alias: 'connected-components',
-    description: 'mark all connected components of the dependency tree as circular',
-    default: false,
-    type: 'boolean'
-  })
-  .option('o', {
-    alias: 'out',
-    description: 'output file',
-    default: '-',
-    defaultDescription: 'stdout',
-    type: 'string',
-    requiresArg: true
-  })
-  .option('t', {
-    alias: 'target',
-    description: 'output format',
-    choices: ['svg', 'dot', 'raw'],
-    default: 'svg',
-    type: 'string',
-    requiresArg: true
-  })
-  .option('engine', {
-    description: 'Graphviz engine (responsible for graph layout)',
-    choices: ['dot', 'circo', 'fdp', 'neato', 'osage', 'twopi'],
-    default: 'dot',
-    type: 'string',
-    requiresArg: true
-  }).argv;
+  .help()
+  .command('viewer <deps>', 'starts a web-server with an interactive graph viewer',
+    (yargs: Argv) => {
+      return yargs.positional('deps', {
+        description: 'path to dependency json file',
+        type: 'string'
+      })
+    },
+    ((args: Arguments) => runServer(args.deps))
+  )
+  .command(['$0', 'generate'], 'generates a dependency graph',
+    (yargs: Argv) =>
+      yargs
+        .option('i', {
+          alias: 'input',
+          description: 'dependency-cruiser JSON file',
+          demandOption: true,
+          type: 'string',
+          requiresArg: true
+        })
+        .option('f', {
+          alias: 'filter',
+          description: 'path for the dependency subgraph',
+          default: '',
+          type: 'string',
+          requiresArg: true
+        })
+        .option('d', {
+          alias: 'depth',
+          description: 'number of child folders to show(0 for infinite)',
+          default: 1,
+          type: 'number',
+          requiresArg: true
+        })
+        .option('D', {
+          alias: 'external-depth',
+          description: 'number of child folders for external dependencies',
+          default: 1,
+          type: 'number',
+          requiresArg: true
+        })
+        .option('e', {
+          alias: 'external-dependencies',
+          description: 'flag to show dependencies which point outside of <filter>',
+          default: false,
+          type: 'boolean',
+        })
+        .option('F', {
+          alias: 'external-filter',
+          description: 'filter path for external dependencies and dependents',
+          type: 'string',
+        })
+        .option('E', {
+          alias: 'external-dependents',
+          description: 'flag to show dependents which point inside of <filter>',
+          default: false,
+          type: 'boolean',
+        })
+        .option('C', {
+          alias: 'connected-components',
+          description: 'mark all connected components of the dependency tree as circular',
+          default: false,
+          type: 'boolean'
+        })
+        .option('o', {
+          alias: 'out',
+          description: 'output file',
+          default: '-',
+          defaultDescription: 'stdout',
+          type: 'string',
+          requiresArg: true
+        })
+        .option('t', {
+          alias: 'target',
+          description: 'output format',
+          choices: ['svg', 'dot', 'raw'],
+          default: 'svg',
+          type: 'string',
+          requiresArg: true
+        })
+        .option('engine', {
+          description: 'Graphviz engine (responsible for graph layout)',
+          choices: ['dot', 'circo', 'fdp', 'neato', 'osage', 'twopi'],
+          default: 'dot',
+          type: 'string',
+          requiresArg: true
+        })
+    , (argv: Arguments) => {
+      const filename = argv.input;
+      const path = argv.filter;
+      const depth = argv.depth >= 0 ? argv.depth : 1;
+      const externalFilter = argv.externalFilter;
+      const externalDependencies = argv.externalDependencies;
+      const externalDependents = argv.externalDependents;
+      const externalDepth = argv.externalDepth >= 0 ? argv.externalDepth : 1;
+      const outputTo = argv.out;
+      const engine = argv.engine;
+      const format = argv.target;
+      const markConnectedComponents = argv.connectedComponents;
 
-const pathSep = '/';
+      run({
+        filename,
+        path,
+        depth,
+        externalDependencies,
+        externalFilter,
+        externalDependents,
+        externalDepth,
+        outputTo,
+        engine,
+        format,
+        markConnectedComponents
+      });
+    }
+  )
+  .argv;
 
-const filename = argv.input;
-const path = argv.filter;
-const depth = argv.depth ? path.split(pathSep).length + argv.depth : 0;
-const externalFilter = argv.externalFilter;
-const externalDependencies = argv.externalDependencies;
-const externalDependents = argv.externalDependents;
-const externalDepth = argv.externalDepth;
-const outputTo = argv.out;
-const engine = argv.engine;
-const format = argv.target;
-const markConnectedComponents = argv.connectedComponents;
-
-run({
-  filename,
-  path,
-  depth,
-  externalDependencies,
-  externalFilter,
-  externalDependents,
-  externalDepth,
-  outputTo,
-  engine,
-  format,
-  markConnectedComponents
-});
--- src/bin/server.ts
+++ src/bin/server.ts
@@ -10,38 +10,49 @@ const PORT = 3000;
 const engine = 'dot';
 const format = 'svg';
 
-const filename = process.argv[2];
-// READ
-const data = JSON.parse(fs.readFileSync(filename, 'utf8'));
-const app = express();
-app.use(express.static(__dirname + '/../../static'));
-app.use(bodyParser.json());
+export function runServer(filename: string) {
+  // READ
+  const data = JSON.parse(fs.readFileSync(filename, 'utf8'));
+  const app = express();
+  app.use(express.static(__dirname + '/../../static'));
+  app.use(bodyParser.json());
 
-app.get('/metadata', (req, res) => res.send(data));
+  app.get('/metadata', (req, res) => res.send(data));
 
-app.post('/svg', (req, res) => {
-  const options = {
-    depth: 1,
-    path: '',
-    externalFilter: undefined,
-    externalDependencies: false,
-    externalDependents: false,
-    externalDepth: 1,
-    markConnectedComponents: false
-  };
-  Object.assign(options, req.body);
-  const dependencies = transform(data, options);
-  const dotGraph = render(dependencies);
-  const viz = new Viz(
-    {Module, render: VizRender});
-  viz.renderString(dotGraph, {engine, format})
-    .then((outputSvg: string) => {
-      res.type('text/svg+xml');
-      res.send(outputSvg);
-    });
+  app.post('/svg', (req, res) => {
+    const options = {
+      depth: 1,
+      path: '',
+      externalFilter: undefined,
+      externalDependencies: false,
+      externalDependents: false,
+      externalDepth: 1,
+      markConnectedComponents: false
+    };
+    Object.assign(options, req.body);
+    const dependencies = transform(data, options);
+    const dotGraph = render(dependencies);
+    const viz = new Viz(
+      {Module, render: VizRender});
+    viz.renderString(dotGraph, {engine, format})
+      .then((outputSvg: string) => {
+        res.type('text/svg+xml');
+        res.send(outputSvg);
+      });
 
-});
+  });
 
-const port = process.env.PORT || PORT;
-app.listen(port);
-console.log(`Server runs on http://localhost:${port}`);
+  const port = process.env.PORT || PORT;
+  app.listen(port);
+  console.log(`Server runs on http://localhost:${port}`);
+}
+
+// run server if file is directly executed via `node server`
+if (module && !module.parent) {
+  const filename = process.argv[2];
+  if (filename) {
+    runServer(filename);
+  } else {
+    console.log('filename to dependency json file needed');
+  }
+}
